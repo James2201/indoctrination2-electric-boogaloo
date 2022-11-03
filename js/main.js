fe65e93ddf55679021a04720b7ef05ca5be3861b
James2201 <112659518+James2201@users.noreply.github.com>
2022-11-03 14:35:05 -0600

    f

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 // USER LOGIN / SIGNUP
 
+let memeber = [];
+
 // HTML VARIABLES
 let signInBtn = document.getElementById('sign-in-btn');
 let signUpBtn = document.getElementById('sign-up-btn');
@@ -8,6 +10,19 @@ let signUpBtn = document.getElementById('sign-up-btn');
 signUpBtn.addEventListener('click', signUpHandler);
 
 function signUpHandler() {
+  let includes = false;
+  let username = document.getElementById("inputusername");
+  let password = document.getElementById("inputpassword");
+  for (let i = 0; i < contacts.length; i++){
+    if (memeber[i].username === username){
+        includes === true
+        alert("identity already in database");
+    } else if (includes === false){
+      memeber.push(newmember(username, password));
+      alert("Sucessful Indoctrination");
+    }
+  }
+  savemember();
   console.log('Sign Up Btn Clicked');
 }
 
@@ -15,5 +30,36 @@ function signUpHandler() {
 signInBtn.addEventListener('click', signInHandler);
 
 function signInHandler() {
+  let index = false;
+  let inputusername = document.getElementById("signinusername");
+  let inputpassword = document.getElementById("signinpassword");
+  for (let i = 0; i < contacts.length; i++){
+    if (memeber[i].username === inputusername && memeber[i].password === inputpassword){
+      alert("Welcome");
+      index = true;
+    } else if (index === false){
+      alert("no")
+    }
+  }
   console.log('Sign In Btn Clicked');
 }
+
+//SUPPORT FUNCTIONS
+
+function newmember(memberusername, memberpassword){
+  return {
+    username: memberusername,
+    password: memberpassword,
+  }
+}
+
+//STORAGE FUNCTIONS 
+
+function savemember(){
+  localStorage.setItem("members", JSON.stringify(memeber));
+} 
+ 
+function loadmember(){
+  let memberstr = localStorage.getItem("members");
+  return JSON.parse(memberstr) ?? [];
+} 
