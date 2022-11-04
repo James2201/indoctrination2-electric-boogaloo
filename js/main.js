e4162eaf9af9988f9990938a391855a749e25e21
James2201 <112659518+James2201@users.noreply.github.com>
2022-11-04 14:19:28 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
-// USER LOGIN / SIGNUP
-
-let memeber = [];
-
+//USER LOGIN / SIGNUP                                          
+ 
+let memeber = loadmember();
+ 
 // HTML VARIABLES
 let signInBtn = document.getElementById('sign-in-btn');
 let signUpBtn = document.getElementById('sign-up-btn');
-
+ 
 // SIGN UP BTN CLICKED
 signUpBtn.addEventListener('click', signUpHandler);
-
+ 
 function signUpHandler() {
   let includes = false;
-  let username = document.getElementById("inputusername");
-  let password = document.getElementById("inputpassword");
+  let username = document.getElementById("inputusername").value;
+  let password = document.getElementById("inputpassword").value;
   for (let i = 0; i < memeber.length; i++){
     if (memeber[i].username === username){
         includes === true
@@ -23,16 +23,15 @@ function signUpHandler() {
     }
   }
   savemember();
-  console.log('Sign Up Btn Clicked');
-}
-
+} 
+ 
 // SIGN IN BTN CLICKED
 signInBtn.addEventListener('click', signInHandler);
-
+ 
 function signInHandler() {
   let index = false;
-  let inputusername = document.getElementById("signinusername");
-  let inputpassword = document.getElementById("signinpassword");
+  let inputusername = document.getElementById("signinusername").value;
+  let inputpassword = document.getElementById("signinpassword").value;
   for (let i = 0; i < memeber.length; i++){
     if (memeber[i].username === inputusername && memeber[i].password === inputpassword){
       alert("Welcome");
@@ -41,24 +40,25 @@ function signInHandler() {
       alert("no")
     }
   }
-  console.log('Sign In Btn Clicked');
-}
-
+  console.log(memeber);
+} 
+ 
 //SUPPORT FUNCTIONS
-
+ 
 function newmember(memberusername, memberpassword){
   return {
     username: memberusername,
     password: memberpassword,
+    
   }
-}
-
+} 
+ 
 //STORAGE FUNCTIONS 
-
+ 
 function savemember(){
   localStorage.setItem("members", JSON.stringify(memeber));
 } 
- 
+  
 function loadmember(){
   let memberstr = localStorage.getItem("members");
   return JSON.parse(memberstr) ?? [];
